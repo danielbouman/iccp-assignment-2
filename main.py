finalized roulette wheel algorithm and probability calculation. only needs correct energy calculation now to work. Added a constant dummy energy for each angle for testing purposes

main.py
@@ -5,11 +5,12 @@
 ## Import modules
 from __future__ import print_function                  # make print function work in python 2.x
 import numpy as np		                               # import numpy
-# import matplotlib.pyplot as plt 			           # plotting tools
+import matplotlib.pyplot as plt 			           # plotting tools
 # from mpl_toolkits.mplot3d import Axes3D		           # plotting tools
 import sys                                             # progress messages
 import running as start                                # startup message
-from new_beads_positions import new_beads_pos          # data export for physcial quantities
+from new_beads_positions import new_beads_pos          # calculate possible new bead positions
+from determine_new_bead import determine_new_bead      # function used to determine the final bead position by comparing the boltzmann factors
 
 ## Global settings
 np.set_printoptions(threshold='nan')		# Do not truncate print
@@ -18,8 +19,8 @@ np.set_printoptions(threshold='nan')		# Do not truncate print
 # Fix Python 2.x.
 try: input = raw_input
 except NameError: pass
-sigma = input('Sigma value of L-J potential (default: 0.5): ') or 0.5
-epsilon = input('Epsilon value of L-J potential (default: 0.5): ') or 1
+sigma = input('Sigma value of L-J potential (default: 0.5): ') or 0.8
+epsilon = input('Epsilon value of L-J potential (default: 0.5): ') or 0.25
 T = input('Temperature, expressed in epsilon (default: 1): ') or 1
 number_of_beads = input('Amount of beads per polymer: ') or 20
 plot_data = input('Plot data? (y/n, default: y): ') or 'y'
@@ -36,11 +37,17 @@ beads_pos = np.zeros((number_of_beads,2),dtype=float)  # initialize all bead pos
 possible_beads_pos = np.zeros((len(angles),2),dtype=float)   # initialize list for all possible positions of the next bead
 
 
+energies = [1,1,0,1,1,1,0,1,0,1]
+
 for N in range(0, number_of_beads-1):
-    possible_bead_pos = new_beads_pos(beads_pos[N,:],angles)  # calculate all possible nodal points
-    beads_pos[N+1,:] = possible_beads_pos[0,:]                              # choose first of all possible nodal points, so polymer is a random 2D walk at the moment. FOR TESTING ONLY!!
+    possible_beads_pos = new_beads_pos(beads_pos[N,:],angles)  # calculate all possible nodal points
+    new_bead_index = determine_new_bead(energies,T)            # determine final new bead
+    beads_pos[N+1,:] = possible_beads_pos[new_bead_index,:]    # add new final new bead to the polymer
 
     plot_beads_pos = np.zeros((N+1,2),dtype=float)                             # this block is used to plot the polymer as it grows, only for tesing purposes
     plot_beads_pos = beads_pos[0:(N+1)]
 
-    
+    plt.plot(plot_beads_pos[:,0],plot_beads_pos[:,1], 'b')
+    plt.plot(plot_beads_pos[:,0],plot_beads_pos[:,1], '.r')
+    plt.axis([-30, 30, -30, 30])
+    plt.show()