corrected formula for the potential energy

calculate_energies.py
@@ -3,7 +3,13 @@
 """
 ## Import libraries
 import numpy as np
-def calculate_energies(pos_nodal_points, angles):
-    energies  = np.zeros(len(angles),dtype=float)
+def calculate_energies(possible_beads_pos,beads_pos,epsilon,sigma):
+    energies  = np.zeros(len(possible_beads_pos),dtype=float) # initialize all possible energies corresponding to each possible new bead position
+    for ii in range(0, len(possible_beads_pos)-1):
+        for iii in range(0,len(beads_pos)-1):
+            abs_distance_squared = np.divide(sum(np.square(np.subtract(possible_beads_pos[ii,:],beads_pos[iii,:]))),np.square(sigma)) # distance alluar
+            V = 4*epsilon*(np.power(abs_distance_squared,-6)-np.power(abs_distance_squared,-3))
+            energies[ii] = energies[ii]+V
 
+    energies = np.divide(energies,1)
     return energies;

main.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt 			           # plotting tools
 import sys                                             # progress messages
 import running as start                                # startup message
 from new_beads_positions import new_beads_pos          # calculate possible new bead positions
+from calculate_energies import calculate_energies      # calculate energies for each new possible bead position
 from determine_new_bead import determine_new_bead      # function used to determine the final bead position by comparing the boltzmann factors
 
 ## Global settings
@@ -22,12 +23,12 @@ except NameError: pass
 sigma = input('Sigma value of L-J potential (default: 0.5): ') or 0.8
 epsilon = input('Epsilon value of L-J potential (default: 0.5): ') or 0.25
 T = input('Temperature, expressed in epsilon (default: 1): ') or 1
-number_of_beads = input('Amount of beads per polymer: ') or 20
+number_of_beads = input('Amount of beads per polymer: ') or 250
 plot_data = input('Plot data? (y/n, default: y): ') or 'y'
 
 
 ## Fixed parameters
-angle_dof = 10                              # Amount of different angles the polymer can move in
+angle_dof = 12                              # Amount of different angles the polymer can move in
 angles = np.linspace(0,2*np.pi,angle_dof)   # Split 2*pi radians up into angle_dof amount of slices
 
 ## Message at simulation start
@@ -37,17 +38,21 @@ beads_pos = np.zeros((number_of_beads,2),dtype=float)  # initialize all bead pos
 possible_beads_pos = np.zeros((len(angles),2),dtype=float)   # initialize list for all possible positions of the next bead
 
 
-energies = [1,1,0,1,1,1,0,1,0,1]
-
 for N in range(0, number_of_beads-1):
     possible_beads_pos = new_beads_pos(beads_pos[N,:],angles)  # calculate all possible nodal points
+    energies = calculate_energies(possible_beads_pos,beads_pos[0:(N+1)],epsilon,sigma)
+    #beads_pos[1,0]
+    #print(energies[0])
+    #print(energies[1])
+    #print(energies[2])
+    #print(energies[3])
     new_bead_index = determine_new_bead(energies,T)            # determine final new bead
     beads_pos[N+1,:] = possible_beads_pos[new_bead_index,:]    # add new final new bead to the polymer
 
-    plot_beads_pos = np.zeros((N+1,2),dtype=float)                             # this block is used to plot the polymer as it grows, only for tesing purposes
-    plot_beads_pos = beads_pos[0:(N+1)]
+    #plot_beads_pos = np.zeros((N+1,2),dtype=float)                             # this block is used to plot the polymer as it grows, only for tesing purposes
+    #plot_beads_pos = beads_pos[0:(N+1)]
 
-    plt.plot(plot_beads_pos[:,0],plot_beads_pos[:,1], 'b')
-    plt.plot(plot_beads_pos[:,0],plot_beads_pos[:,1], '.r')
-    plt.axis([-30, 30, -30, 30])
-    plt.show()
+plt.plot(beads_pos[:,0],beads_pos[:,1], 'b')
+plt.plot(beads_pos[:,0],beads_pos[:,1], '.r')
+#plt.axis([-1*(number_of_beads), number_of_beads, -1*(number_of_beads), number_of_beads])
+plt.show()