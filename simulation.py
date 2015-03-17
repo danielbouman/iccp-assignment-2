35836e9a9379bc95ea35417f74bf78f2152380f9
Daniel Bouman <d.bouman1@gmail.com>
2015-03-17 09:05:39 +0000

    implemented list tracking, speed improvement of 10x

--- simulation.py
+++ simulation.py
@@ -4,6 +4,7 @@ import numpy as np		                            # import numpy
 from new_beads_positions import new_beads_pos       # calculate possible new bead positions
 from calculate_energies import calculate_energies   # calculate energies for each new possible bead position
 from determine_new_bead import determine_new_bead   # function used to determine the final bead position by comparing the boltzmann factors
+import list_tracking as track
 
 def user_input():
     sigma = input('Sigma value of L-J potential (default: 0.5): ') or 0.8
@@ -15,31 +16,23 @@ def user_input():
 
 def start(number_of_beads,sigma,epsilon,T):
     ## Fixed parameters
-    angle_dof = 6                               # Amount of different angles the polymer can move in
+    angle_dof = 36                              # Amount of different angles the polymer can move in
     angles = np.linspace(0,2*np.pi,angle_dof)   # Split 2*pi radians up into angle_dof amount of slices
     
-    beads_pos = np.zeros((number_of_beads,2),dtype=float)  # initialize all bead positions
-    possible_beads_pos = np.zeros((len(angles),2),dtype=float)   # initialize list for all possible positions of the next bead
+    excisting_bead_pos = np.zeros((number_of_beads,2),dtype=float)  # initialize all bead positions
+    candidate_bead_pos = np.zeros((len(angles),2),dtype=float)      # initialize list for all possible positions of the next bead
 
     sigma_squared = np.square(sigma)
     
+    track.init(int(np.ceil(number_of_beads/2))) # initialize tracking
+    
     for N in range(0, number_of_beads):
-        possible_beads_pos = new_beads_pos(beads_pos[N-1,:],angles)  # calculate all possible nodal points
-        energies = calculate_energies(possible_beads_pos,beads_pos[0:N],epsilon,sigma_squared)
-        #beads_pos[1,0]
-        #print(energies[0])
-        #print(energies[1])
-        #print(energies[2])
-        #print(energies[3])
-        new_bead_index = determine_new_bead(energies,T)            # determine final new bead
-        beads_pos[N,:] = possible_beads_pos[new_bead_index,:]    # add new final new bead to the polymer
-        # plot_beads_pos = np.zeros((N+1,2),dtype=float)                             # this block is used to plot the polymer as it grows, only for tesing purposes
-        # plot_beads_pos = beads_pos[0:(N+1)]
-
-        # plt.plot(plot_beads_pos[:,0],plot_beads_pos[:,1], 'b')
-        # plt.plot(plot_beads_pos[:,0],plot_beads_pos[:,1], '.r')
-        # plt.show()
-    return beads_pos
+        candidate_bead_pos = new_beads_pos(excisting_bead_pos[N-1,:],angles)    # calculate all possible nodal points
+        energies = calculate_energies(candidate_bead_pos,excisting_bead_pos[0:N],epsilon,sigma_squared) # calculate energies
+        new_bead_index = determine_new_bead(energies,T)                         # determine final new bead
+        excisting_bead_pos[N,:] = candidate_bead_pos[new_bead_index,:]          # add new final new bead to the polymer
+        track.store(excisting_bead_pos[N,:],N)
+    return excisting_bead_pos
 
 def plot(beads_pos):
     import matplotlib.pyplot as plt 			           # plotting tools
