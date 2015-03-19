fce1d392da04f73b07fb565f3a2811c5a712f644
Daniel Bouman <d.bouman1@gmail.com>
2015-03-19 10:06:53 +0000

    fortran lj_energies written for 2d without list tracking

--- test.f90
+++ test.f90
@@ -1,10 +1,24 @@
-subroutine func(a,size,b)
+subroutine func(pos,candidate_pos,N_candidates,N_existing,sigma_squared,epsilon,energies)
     implicit none
-   
-    integer, intent(in) :: size
-    real(8), dimension(size,2), intent(in) :: a
-    real(8), dimension(size), intent(out) :: b
     
-    b = a(:,2)**(-1)
+    integer, intent(in) :: N_candidates, N_existing
+    real(8), intent(in) :: sigma_squared, epsilon
+    
+    real(8), dimension(N_existing,2), intent(in) :: pos
+    real(8), dimension(N_candidates,2), intent(in) :: candidate_pos
+    real(8), dimension(N_candidates), intent(out) :: energies
+    real(8) :: V
+    
+    real(8) :: abs_distance_squared
+    integer :: i,j
+    
+    do i=1,N_candidates
+        do j=1,N_existing
+            abs_distance_squared = sum((candidate_pos(i,:) - pos(j,:))**2) / sigma_squared
+            V = 4*epsilon*( (abs_distance_squared)**(-6) - (abs_distance_squared)**(-3))
+            energies(i) = energies(i)+V
+        end do
+        print *, i
+    end do
     
 end subroutine
--- test_f2py.py
+++ test_f2py.py
@@ -1,10 +1,17 @@
 import test
 import numpy as np
 
-my_array = np.ones((3,2))
-my_array[:,0] = range(3)
-my_array[:,1] = range(3,6)
-my_array_size = 3
+sigma_squared = 0.64
+epsilon = 0.25
+N_existing = 3
+N_candidates = 5
+pos = np.ones((N_existing,2))
+candidate_pos = np.ones((N_candidates,2))
+pos[:,0] = range(3)
+pos[:,1] = range(3,6)
+candidate_pos[:,0] = range(5)
+candidate_pos[:,1] = range(5,10)
 
-square = test.func(my_array,my_array_size)
-print(square)
+# square = test.func(my_array,my_array_size)
+energies = test.func(pos,candidate_pos,sigma_squared,epsilon,N_candidates,N_existing)
+print(energies)
