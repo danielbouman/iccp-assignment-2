bea53f2a71a49978394746244df85482cf54f796
Kenneth Goodenough <kdgoodenough@gmail.com>
2015-03-26 15:03:01 +0100

    updated main so that we can run ensembles, also added calculations of weightfactors

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ from datetime import datetime   # timer functions
 import simulation               # simulation module        
 
 # Simulation
-sigma, epsilon, T, Nbeads, plot_data = simulation.user_input()  # User input for simualation variables
+sigma, epsilon, T, amount_of_beads, plot_data, amount_of_polymers = simulation.user_input()  # User input for simualation variables
+end_to_end_distance = np.zeros((amount_of_polymers,1),dtype=float)    # initialize all end_to_end distances
+
 start_time=datetime.now() # =========== Start timer                                             # Startup message
-beads_pos = simulation.start(Nbeads,sigma,epsilon,T)            # Start simulation
+for ii in range(0, number_of_beads):
+    beads_pos,end_to_end_distance[ii] = simulation.start(amount_of_beads,sigma,epsilon,T)            # Start simulation
+
 print(datetime.now() - start_time) # =========== End timer
 if plot_data == 'y':                                            # Plot when chosen
     simulation.plot(beads_pos)
--- simulation.py
+++ simulation.py
@@ -15,7 +15,8 @@ def user_input():
     T = 1
     number_of_beads = 150
     plot_data = 'n'
-    return float(sigma), float(epsilon), float(T), int(number_of_beads), plot_data
+    amount_of_polymers = 100
+    return float(sigma), float(epsilon), float(T), int(number_of_beads), plot_data,int(amount_of_polymers);
 
 def start(number_of_beads,sigma,epsilon,T):
     ## Fixed parameters
@@ -26,13 +27,17 @@ def start(number_of_beads,sigma,epsilon,T):
     existing_pos = np.zeros((number_of_beads,2),dtype=float)    # initialize all bead positions
     candidate_pos = np.zeros((len(angles),2),dtype=float)       # initialize list for all possible positions of the next bead
     angle_last_bead = 0
-    
+    total_weight_factor = 1
+
     for N in range(1, number_of_beads):
         candidate_pos = new_bead.positions(existing_pos[N-1,:],angles)  # calculate all possible nodal points
         energies = lj_energy.func(existing_pos[0:N,:],candidate_pos,sigma_squared,epsilon,angle_dof,N) # calculate energies
-        new_bead_index = new_bead.roulette(energies,T)         # determine final new bead
+        new_bead_index,weight_factor = new_bead.roulette(energies,T)         # determine final new bead
+        total_weight_factor = weight_factor*total_weight_factor
         existing_pos[N,:] = candidate_pos[new_bead_index,:]    # add new final new bead to the polymer
-    return existing_pos
+
+    end_to_end_distance = total_weight_factor*np.square(sum(np.square(existing_pos[0,:]-existing_pos[end,:])))
+    return existing_pos,end_to_end_distance
 
 def plot(beads_pos):
     import matplotlib.pyplot as plt     # plotting tools
