1f8f5cf0c35cce974a92ac4737e32fb441a7dd77
Koding <d.bouman1@gmail.com>
2015-03-26 13:37:19 +0000

    fixed merge

--- simulation.py
+++ simulation.py
@@ -28,15 +28,9 @@ def start(number_of_beads,sigma,epsilon,T):
     angle_last_bead = 0
     
     for N in range(1, number_of_beads):
-<<<<<<< HEAD
         candidate_pos = new_bead.positions(existing_pos[N-1,:],angles)  # calculate all possible nodal points
         energies = lj_energy.func(existing_pos[0:N,:],candidate_pos,sigma_squared,epsilon,angle_dof,N) # calculate energies
         new_bead_index = new_bead.roulette(energies,T)         # determine final new bead
-=======
-        candidate_pos,angle_updated = new_beads_pos(existing_pos[N-1,:],angles)  # calculate all possible nodal points
-        energies = lj_energy.func(existing_pos[0:N,:],candidate_pos,sigma_squared,epsilon,angle_dof,N,angle_updated,angle_last_bead,bending_energy) # calculate energies
-        new_bead_index = determine_new_bead(energies,T)         # determine final new bead
->>>>>>> a1de3ce5ea8c82aa990a7d84895dcffa73751178
         existing_pos[N,:] = candidate_pos[new_bead_index,:]    # add new final new bead to the polymer
     return existing_pos
 
