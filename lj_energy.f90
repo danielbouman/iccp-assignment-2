931acb3ab974c91b58acfb576feb1ae0e81d4e6a
Koding <d.bouman1@gmail.com>
2015-04-04 07:45:54 +0000

    Fixed bending energy in lj_energy fortran code and compiled new python module. Run f2py on Windows for python windows specific Python module

--- lj_energy.f90
+++ lj_energy.f90
@@ -3,15 +3,16 @@ subroutine func(pos,candidate_pos,N_candidates,N_existing,sigma_squared,epsilon,
     
     integer, intent(in) :: N_candidates, N_existing
     real(8), intent(in) :: sigma_squared, epsilon, last_angle, bend_energy
-    
     real(8), dimension(N_existing,2), intent(in) :: pos
     real(8), dimension(N_candidates,2), intent(in) :: candidate_pos
     real(8), dimension(N_candidates), intent(in) :: possib_angles
-    real(8), dimension(N_candidates), intent(out) :: energies
-    real(8) :: V
     
-    real(8) :: abs_distance_squared
+    real(8), dimension(N_candidates), intent(out) :: energies
+    
+    real(8) :: V, pi, abs_distance_squared
     integer :: i,j
+    
+    pi = 3.1415927
     
     do i=1,N_candidates
         do j=1,N_existing-1
@@ -19,7 +20,7 @@ subroutine func(pos,candidate_pos,N_candidates,N_existing,sigma_squared,epsilon,
             V = 4*epsilon*( (abs_distance_squared)**(-6) - (abs_distance_squared)**(-3))
             energies(i) = energies(i)+V
         end do
-        energies(i) = energies(i)+bend_energy*sin(0.5*(possib_angles(i)-last_angle),4.*atan(1.))**2
+        energies(i) = energies(i)+bend_energy*sin(0.5*modulo( (possib_angles(i)-last_angle),pi) )**2
     end do
     
 end subroutine
